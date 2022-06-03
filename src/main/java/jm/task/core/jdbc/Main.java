26688dd3d3eced08cc3a4b5aa004d79024fe8242
Vladimir Petrov <ElaijaEc@yandex.ru>
2022-06-03 14:38:27 +0400

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,18 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ivan","Ivanov", (byte) 19);
+        userService.saveUser("Anna","Petrova", (byte) 28);
+        userService.saveUser("Vladimir","Kutuzov", (byte) 69);
+        userService.getAllUsers();
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,37 @@
+package jm.task.core.jdbc.util;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_113";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Admin";
+
+
+    static Connection connection;
+    public static Connection con(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+//        finally {
+//            if(connection!=null){
+//                try {
+//                    connection.close();
+//                } catch (SQLException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }
+        return connection;
+    }
+}
