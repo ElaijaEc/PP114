9312e5f6dac7d19ed5c636e21421cf295da8b983
Vladimir Petrov <ElaijaEc@yandex.ru>
2022-06-05 22:41:50 +0400

    ЩЬП

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
-        //userService.saveUser("Ivan","Ivanov", (byte) 19);
-        //userService.saveUser("Anna","Petrova", (byte) 28);
-        //userService.saveUser("Vladimir","Kutuzov", (byte) 69);
-        //userService.getAllUsers();
-        //userService.removeUserById(1);
-        //userService.cleanUsersTable();
-        //userService.dropUsersTable();
+        userService.saveUser("Ivan","Ivanov", (byte) 19);
+        userService.saveUser("Anna","Petrova", (byte) 28);
+        userService.saveUser("Vladimir","Kutuzov", (byte) 69);
+        userService.getAllUsers();
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
